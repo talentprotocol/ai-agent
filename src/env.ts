48a2cee8ef7bd3376be826fd2d2f7cd511b0dcb6
PaoloRollo <paolo.rollo1997@gmail.com>
2023-11-10 15:56:56 +0100

    feat: add scripts and coerce.

--- src/env.ts
+++ src/env.ts
@@ -2,9 +2,9 @@ import z from "zod";
 
 const envSchema = z.object({
   OPENAI_API_KEY: z.string(),
-  PORT: z.number().optional().default(8080),
-  RATE_LIMIT_DURATION: z.number().optional().default(60000),
-  RATE_LIMIT_MAX: z.number().optional().default(60),
+  PORT: z.coerce.number().optional().default(8080),
+  RATE_LIMIT_DURATION: z.coerce.number().optional().default(60000),
+  RATE_LIMIT_MAX: z.coerce.number().optional().default(60),
 });
 
 export const env = envSchema.parse(process.env);
