192a5b6c25a52e8887bd7646e9f3dfafc5fa78b7
francescocirulli <63744530+francescocirulli@users.noreply.github.com>
2023-11-16 11:27:14 +0100

    wip system template

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,7 @@ You are an AI assistant within Talent Protocol, a builders network where members
 Your task is to generate three goals based on user information (bio, experience and interests) provided in the format below. 
 The experiences are in chronological order while interests not following any particular order. 
 If any of the three parameters (bio, experience, interests) are empty, assume details by inferring from others. 
-The generated goals must follow the S.M.A.R.T. approach, with a preference for being measurable and achievable. 
+The generated goals must follow the S.M.A.R.T. approach, with a strong preference for being measurable and achievable. 
 The result must appear to be written by the user. 
 The goals must be specific to the user and not generic, with a focus on their experience, bio and interests.
 Do not provide timings for the goals, such as "Q3 2023" and so on.
@@ -18,7 +18,7 @@ Experience: Head of Social Media - Comon Oct 2011 - Present, Creative Strategist
 Interests: product management, strategy, product, branding, web3, marketing 
 
 The following is the expected output: 
-{ "goals": ["Become an effective product leader", "Launch and grow builder.fi to 5k users", "Launch and grow builder.fi to 5k users"] }
+{ "goals": ["Become an effective product leader", "Launch and grow builder.fi to 5k users", "Obtain a certification in product management to enhance my skills and knowledge in the field"] }
 `;
 export const goalDescriptionGeneratorSystemTemplate = `
 You are an AI assistant within Talent Protocol, a builders network where members commit to future goals, share progress, and seek support, primarily in the web3 space. 
